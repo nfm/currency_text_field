c542bbc4c4fcb64d76da4a7f1ca6710061307b94
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-10-20 10:39:39 +1100

    Fixed copying of initializer in install.rb

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 puts 'Copying files...'
 
 src_dir = File.join(File.dirname(__FILE__), 'lib', 'installed_files')
-FileUtils.copy(File.join(src_dir, 'currency_text_field_initializer.rb', "#{Rails.root}/config/initializers/"))
+FileUtils.copy(File.join(src_dir, 'currency_text_field_initializer.rb'), "#{Rails.root}/config/initializers/currency_text_field_initializer.rb")
 
 puts 'Files copied - installation complete!'
